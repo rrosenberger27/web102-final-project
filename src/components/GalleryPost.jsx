693ab67be7df5e8c3467df92eb1a17256332d31b
Riley <rileyrosenberger23@gmail.com>
2025-08-03 14:30:50 -0400

    initial upvote updates

--- src/components/GalleryPost.jsx
+++ src/components/GalleryPost.jsx
@@ -1,12 +1,28 @@
 import React from 'react'
 import '../styles/GalleryPost.css'
+import { useState, useEffect } from 'react'
+import supabase from '../client.js'
 
 // only show creation time, title, and upvotes count -- need to be able to update the upvotes count
 
 const GalleryPost = ({ post }) => {
+    const [upvotes, setUpvotes] = useState(post.upvotes)
+    useEffect(() => {
+        supabase.from('gallery').update({ upvotes: upvotes }).eq('id', post.id).select()
+        .then(({ data, error }) => {
+            if (error) {
+                console.error(error)
+            } else {
+                console.log(data)
+            }
+        })
+    }, [upvotes])
   return (
     <div className='gallery-post' style={{ backgroundColor: post.background_color, color: post.text_color }}>
       <h1>{post.title}</h1>
+      <p>Created: {new Date(post.created_at).toLocaleDateString()}</p>
+      <p>Upvotes: {upvotes}</p>
+     <button className='upvote-button-gallery' style={{ backgroundColor: post.text_color, color: post.background_color }} onClick={() => setUpvotes(upvotes + 1)}>&#8593;</button>
     </div>
   )
 }
